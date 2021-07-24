0f42c1eb8a8a46469b5fe2bfd6943d24efc10859
namme18 <nammeselarom@gmail.com>
2021-07-24 16:56:39 +0800

    testing on air

--- server.js
+++ server.js
@@ -28,6 +28,16 @@ app.use('/subs', require('./routes/subs'));
 //error handler
 app.use(errorHandler);
 
+//serve static assets if production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
